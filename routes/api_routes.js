384f7dad2abce157be9c6a3bd02197e1215b384f
Sam Fan <samisfan@msn.com>
2021-02-03 14:41:14 -0700

    seed file working, homepage functioning, not loading new or continue yet

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,10 +1,59 @@
 const router = require("express").Router();
-const Cardio = require("../models/cardio.js");
-const Resistance = require("../models/resistance.js");
+const { Workout } = require("../models");
 
-// GET all route to display recent workouts
-// PUT route, add exercises to most recent workout plan
-// POST route, add new exercises to a new workout plan
+// get route for last workout
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      // internal server error
+      res.status(500).json(err);
+    });
+});
+
+// GET all route to display recent workouts, sorted by date
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// PUT route, add exercises
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// POST route, create a new workout
+router.post("/api/workouts", (req, res) => {
+  const { body } = req;
+  Workout.create(body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 // GET route, combined weight from the past seven workouts on the stats page
 // GET route, total duration from the past seven workouts on the stats page
 
